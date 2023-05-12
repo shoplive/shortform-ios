36ab19879a28976e625011dfb216187cb91c1326
hassan0424 <hassan@shoplive.cloud>
2023-05-12 21:32:54 +0900

    no message

--- Package.swift
+++ Package.swift
@@ -10,16 +10,18 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ShopliveShortformSDK",
-            targets: ["ShopliveShortformSDKTargets"]),
+            targets: ["ShopliveShortformSDK"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/shoplive/common-ios.git", .upToNextMajor(from: "1.4.0"))
+        .package(url: "https://github.com/shoplive/common-ios.git", .upToNextMajor(from: "1.4.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(name: "ShopliveShortformSDK",
-                      path: "./Frameworks/ShopliveShortformSDK.xcframework"
-                     ),
+                      path: "./Frameworks/ShopliveShortformSDK.xcframework"),
+        .target(name: "ShopLiveSDKCommon",
+                dependencies: [.product(name: "ShopliveSDKCommon", package: "common-ios")])
     ]
 )
+
