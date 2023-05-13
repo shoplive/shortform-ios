c688dbb18bc68ca12b2efa561863d4034034f636
hassan0424 <131642763+hassan0424@users.noreply.github.com>
2023-05-13 18:13:42 +0900

    package.swift 파일 교체 및 프레임워크 교체 (#1)

---
+++ Package.swift
@@ -0,0 +1,30 @@
+
+
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ShopliveShortformSDK",
+    platforms: [.iOS(.v11)],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "ShopliveShortformSDK",
+            targets: ["ShopLiveShortformSDK","ShopLiveSDKCommonTarget"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/shoplive/common-ios.git", .upToNextMajor(from: "1.4.0")),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .binaryTarget(name: "ShopLiveShortformSDK",
+                      path: "./Frameworks/ShopLiveShortformSDK.xcframework"),
+        
+        .target(name: "ShopLiveSDKCommonTarget",
+                dependencies: [.product(name: "ShopliveSDKCommon", package: "common-ios")])
+    ]
+)
+
---
+++ Sources/ShopLiveSDKCommonTarget/ShopLiveSDKCommonTarget.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by sangmin han on 2023/05/12.
+//
+
+import Foundation
