97ccc994055538b873500c8d164143c918aa9a1e
hassan0424 <hassan@shoplive.cloud>
2023-05-15 18:09:45 +0900

    framework changed

---
+++ Frameworks/ShopliveSDKCommon.xcframework/ios-arm64_x86_64-simulator/ShopliveSDKCommon.framework/Headers/matrix_common_ios.h
@@ -0,0 +1,18 @@
+//
+//  matrix_common_ios.h
+//  matrix-common-ios
+//
+//  Created by Vincent on 1/24/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for matrix_common_ios.
+FOUNDATION_EXPORT double matrix_common_iosVersionNumber;
+
+//! Project version string for matrix_common_ios.
+FOUNDATION_EXPORT const unsigned char matrix_common_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <matrix_common_ios/PublicHeader.h>
+
+
