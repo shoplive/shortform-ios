f6cf9cdc9f4635161e6e0a269cfdd10efe950327
hassan0424 <hassan@shoplive.cloud>
2023-08-02 14:20:13 +0900

    package.swift changed

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         .library(
             name: "ShopliveShortformSDK",
-            targets: ["ShopLiveShortformSDK","ShopLiveShortformUploadSDK"])
+            targets: ["ShopLiveShortformSDK"])
     ],
     
     targets: [
